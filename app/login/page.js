c0756b683732ea89a02f38a2a4a33fccae1bce75
amitdebnath76 <debnath4455@gmail.com>
2024-11-05 19:07:58 +0600

    create login page,and handle input field

--- app/login/page.js
+++ app/login/page.js
@@ -1,8 +1,27 @@
+'use client'
 import Link from 'next/link'
-import React from 'react'
+import { useState } from 'react'
 
 
 const Login = () => {
+  
+const [email,setEmail]=useState();
+const [password,setPassword] = useState()
+const [error,setError] = useState(false)
+
+const handleLogin=(event)=>{
+  event.preventDefault();
+  if(!email || !password){
+    setError(true)
+    
+  }else{
+    setError(false)
+  }
+  setEmail('');
+  setPassword('')
+  console.log(email,password)
+}
+
   return (
     <>
     <div className="hero bg-gradient-to-r from-sky-500 to-indigo-500 min-h-screen">
@@ -19,19 +38,25 @@ const Login = () => {
           <label className="label">
             <span className="label-text">Email</span>
           </label>
-          <input type="email" placeholder="email" className="input input-bordered" required />
+          <input value={email} onChange={(event)=>setEmail(event.target.value)} type="email" placeholder="email" className="input input-bordered" required />
+          {
+            error && !email && <span className='text-red-500'>Please enter a valid Email</span>
+          }
         </div>
         <div className="form-control">
           <label className="label">
             <span className="label-text">Password</span>
           </label>
-          <input type="password" placeholder="password" className="input input-bordered" required />
+          <input value={password} onChange={(event)=>setPassword(event.target.value)} type="password" placeholder="password" className="input input-bordered" required />
+          {
+            error && !password  && <span className='text-red-500'>Please enter a valid Password</span>
+          }
           <label className="label">
             <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
           </label>
         </div>
         <div className="form-control mt-6">
-          <button className="btn btn-primary">Login</button>
+          <button onClick={handleLogin} className="btn btn-primary">Login</button>
         </div>
       </form>
     </div>
