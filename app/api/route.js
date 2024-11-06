923acb8ce67d2db91d85ea815dcc459b63915a7a
amitdebnath76 <debnath4455@gmail.com>
2024-11-07 01:10:55 +0600

    create POST Api for login form

--- app/api/route.js
+++ app/api/route.js
@@ -3,21 +3,30 @@ import { restaurentSchema } from "@/app/lib/restaurentModel";
 import mongoose from "mongoose";
 import { NextResponse } from "next/server";
 
-export async function GET(){
-    await mongoose.connect(connectionStr)
+export async function GET() {
+  await mongoose.connect(connectionStr);
 
-    const data = await restaurentSchema.find()
-    console.log(data)
-    return NextResponse.json({result:data})
+  const data = await restaurentSchema.find();
+  console.log(data);
+  return NextResponse.json({ result: data });
 }
 
 export async function POST(request) {
-    const payload = await request.json();
-    await mongoose.connect(connectionStr)
-    let restaurent = new restaurentSchema(payload)
-    const result = await restaurent.save()
-    console.log(payload)
-    return NextResponse.json({result,success:true})
+  const payload = await request.json();
+  await mongoose.connect(connectionStr);
+  let result;
+  if (payload.login) {
+    result = await restaurentSchema.findOne({
+      email: payload.email,
+      password: payload.password,
+    });
+  } else {
+    let restaurent = new restaurentSchema(payload);
+    result = await restaurent.save();
+  }
+
+  console.log(payload);
+  return NextResponse.json({ result, success: true });
 }
 // export async function POST(){
 //     return NextResponse.json({return:true})
--- app/login/page.js
+++ app/login/page.js
@@ -1,69 +1,105 @@
-'use client'
-import Link from 'next/link'
-import { useState } from 'react'
-
+"use client";
+import Link from "next/link";
+import { useState } from "react";
 
 const Login = () => {
-  
-const [email,setEmail]=useState();
-const [password,setPassword] = useState()
-const [error,setError] = useState(false)
+  const [email, setEmail] = useState();
+  const [password, setPassword] = useState();
+  const [error, setError] = useState(false);
 
-const handleLogin=(event)=>{
-  event.preventDefault();
-  if(!email || !password){
-    setError(true)
-    
-  }else{
-    setError(false)
-  }
-  setEmail('');
-  setPassword('')
-  console.log(email,password)
-}
+  const handleLogin = async (event) => {
+    event.preventDefault();
+    if (!email || !password) {
+      setError(true);
+    } else {
+      setError(false);
+    }
+
+    console.log(email, password);
+
+    let response = await fetch("http://localhost:3000/api", {
+      method: "POST",
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify({ email, password, login: true }),
+    });
+    response = await response.json();
+    if (response.success) {
+      alert("Your Login is successful");
+      setEmail("");
+      setPassword("");
+    }
+  };
 
   return (
     <>
-    <div className="hero bg-gradient-to-r from-sky-500 to-indigo-500 min-h-screen">
-  <div className="hero-content flex-col lg:flex-row-reverse">
-    <div className="text-center lg:text-left">
-      <h1 className="text-5xl font-bold">Login now!</h1>
-      <p className="py-6">
-        Do not have Account? <Link className=' text-blue-700 text-2xl font-extrabold' href="/signup">Sign Up</Link>
-      </p>
-    </div>
-    <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
-      <form className="card-body">
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Email</span>
-          </label>
-          <input value={email} onChange={(event)=>setEmail(event.target.value)} type="email" placeholder="email" className="input input-bordered" required />
-          {
-            error && !email && <span className='text-red-500'>Please enter a valid Email</span>
-          }
+      <div className="hero bg-gradient-to-r from-sky-500 to-indigo-500 min-h-screen">
+        <div className="hero-content flex-col lg:flex-row-reverse">
+          <div className="text-center lg:text-left">
+            <h1 className="text-5xl font-bold">Login now!</h1>
+            <p className="py-6">
+              Do not have Account?{" "}
+              <Link
+                className=" text-blue-700 text-2xl font-extrabold"
+                href="/signup"
+              >
+                Sign Up
+              </Link>
+            </p>
+          </div>
+          <div className="card bg-base-100 w-full max-w-sm shrink-0 shadow-2xl">
+            <form className="card-body">
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Email</span>
+                </label>
+                <input
+                  value={email}
+                  onChange={(event) => setEmail(event.target.value)}
+                  type="email"
+                  placeholder="email"
+                  className="input input-bordered"
+                  required
+                />
+                {error && !email && (
+                  <span className="text-red-500">
+                    Please enter a valid Email
+                  </span>
+                )}
+              </div>
+              <div className="form-control">
+                <label className="label">
+                  <span className="label-text">Password</span>
+                </label>
+                <input
+                  value={password}
+                  onChange={(event) => setPassword(event.target.value)}
+                  type="password"
+                  placeholder="password"
+                  className="input input-bordered"
+                  required
+                />
+                {error && !password && (
+                  <span className="text-red-500">
+                    Please enter a valid Password
+                  </span>
+                )}
+                <label className="label">
+                  <a href="#" className="label-text-alt link link-hover">
+                    Forgot password?
+                  </a>
+                </label>
+              </div>
+              <div className="form-control mt-6">
+                <button onClick={handleLogin} className="btn btn-primary">
+                  Login
+                </button>
+              </div>
+            </form>
+          </div>
         </div>
-        <div className="form-control">
-          <label className="label">
-            <span className="label-text">Password</span>
-          </label>
-          <input value={password} onChange={(event)=>setPassword(event.target.value)} type="password" placeholder="password" className="input input-bordered" required />
-          {
-            error && !password  && <span className='text-red-500'>Please enter a valid Password</span>
-          }
-          <label className="label">
-            <a href="#" className="label-text-alt link link-hover">Forgot password?</a>
-          </label>
-        </div>
-        <div className="form-control mt-6">
-          <button onClick={handleLogin} className="btn btn-primary">Login</button>
-        </div>
-      </form>
-    </div>
-  </div>
-</div>
+      </div>
     </>
-  )
-}
+  );
+};
 
-export default Login
+export default Login;
